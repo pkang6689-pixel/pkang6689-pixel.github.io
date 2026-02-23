573b5018c44e3ef6cae2fcae58d88d362cd5ddcd
hitler <pkang6689@gmail.com>
2026-02-23 12:01:45 +0800

    Some more translation fixes

--- verify_translations.py
+++
@@ -1,7 +0,0 @@
-import json
-d = json.load(open('geometry_translation_dict.json', encoding='utf-8'))
-print(len(d), 'total translations')
-checks = ['Our Courses', 'Mathematics', 'Science', 'High School: Geometry', 
-           'Unit 1', 'Unit 8 Test', 'Submit Answer', 'Back to Geometry', 'Get Started Today']
-for c in checks:
-    print(f'  {c}: {d.get(c, "MISSING")}')
